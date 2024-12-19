2614b594e1df18bce4f5c6c46eb70ec212612117
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-19 16:50:14 -0300

    feat:Using the prisma client to make the service funtional

---
+++ src/user/user.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from "@nestjs/common";
+import { ExceptionsHandler } from "@nestjs/core/exceptions/exceptions-handler";
+import { Prisma, Transacoes, User } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+import { PrismaService } from "prisma/prisma.service";
+
+interface UserThings{
+    id?:number;
+    name?:string;
+    email?:string;
+    password?:string;
+    transacoes:Transacoes[];
+    data?:{
+        [key:string]:any
+    };
+};
+
+
+@Injectable()
+export class UserService{
+    private readonly prisma: Prisma.UserDelegate<DefaultArgs>;
+    constructor(private readonly pr:PrismaService){
+        this.prisma = pr.user;
+    };
+
+    public async Insert({name,email,password}:UserThings):Promise<User>{
+        try{
+            const tentaInsert = await this.prisma.create({
+                data:{
+                    name:name,
+                    email:email,
+                    password:password
+                }
+            });
+
+            if(!tentaInsert){
+                console.error("Error to insert the data into the db!");
+              throw new ExceptionsHandler();
+            };
+            
+            return tentaInsert;
+            
+        } catch(err){
+            console.error(err);
+            throw new ExceptionsHandler();
+        };
+    };
+    
+    public async SelectAll():Promise<User[]>{
+        try{
+            const tentaSelect = await this.prisma.findMany();
+            if(!tentaSelect){
+                console.error("Error to select all the data into the db!");
+                throw new ExceptionsHandler();
+
+            };
+            return tentaSelect;
+        }catch(err){
+            console.error(err);
+            throw new ExceptionsHandler();
+        };
+    };
+    
+    public async SelectOne({id}:UserThings):Promise<User>{
+        try{
+            const tentaSelectOne = await this.prisma.findFirst({
+                where:{
+                    id:id
+                }
+            });
+            if(!tentaSelectOne){
+                console.error("Error to select all the data into the db!");
+                throw new ExceptionsHandler();
+                
+            };
+            return tentaSelectOne;
+        } catch(err){
+            console.error(err);
+            throw new ExceptionsHandler();
+        };
+    };  
+
+};
