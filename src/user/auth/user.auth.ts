83949c5501d58bfb8bc1029def86e5abc6434619
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-19 17:13:20 -0300

    feat:Add Nest.js JwtService 100% avaible to be in the controller

---
+++ src/user/auth/user.auth.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@nestjs/common";
+import { ExternalExceptionsHandler } from "@nestjs/core/exceptions/external-exceptions-handler";
+import { JwtService } from "@nestjs/jwt";
+
+@Injectable()
+export class Userauth{
+    constructor(private readonly jwtService:JwtService){};
+
+    private createNewToken(argument:any):string{
+        try{
+            const novoArgs = this.jwtService.sign({argument});
+            if(!novoArgs){
+                console.error("Error to make the argument a JWT token!");
+                throw new ExternalExceptionsHandler();
+            };
+            return novoArgs;
+        } catch(err){
+            console.error(err);
+            throw new ExternalExceptionsHandler();
+        };
+    };
+
+    private async checkTheToken(token:string):Promise<object>{
+        try{
+            const verificaToken = await this.jwtService.verify(token);
+
+            if(!verificaToken){
+                console.error("Error to verify the JWT token!");
+                throw new ExternalExceptionsHandler();
+            };
+
+            return verificaToken;
+        }catch(err){
+            console.error(err);
+            throw new ExternalExceptionsHandler();
+        };
+    };
+
+    private async makeTheTokenAnArgument(token:string){
+        try{
+            this.checkTheToken(String(token));
+            await this.jwtService.decode(token)
+        }catch(err){
+            console.error(err);
+            throw new ExternalExceptionsHandler();
+        };
+    };
+
+};
