c84369f6a7a9af068362f81e532251a801274480
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-19 16:51:37 -0300

    feat: Add new DTO to control the method post with more precision

---
+++ src/user/DTO/user.dto.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
+
+@Injectable()
+export class  CreationUser{
+    @IsString({message:"The name needs to be a string"})
+    @MaxLength(20, {message:"The name only can have 20 caractheres!"})
+    name:string;
+
+    @IsEmail(undefined, {message:"The email needs to follow this: **youremail@yourpriver.com**"})
+    email:string;
+
+    @IsString({message:"The password needs to be a string!"})
+    @MinLength(8,{message:"The password needs to have 8 caractheres!"})
+    password:string;
+
+};
