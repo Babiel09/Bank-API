abfcbc511a5c70bc703eade02f6d7a17e1af2626
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-19 16:52:38 -0300

    feat: Add the import of the PrismaService in the app.module to make it avaible to the my other class's

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { UserModule } from './user/user.module';
+import { BullModule } from '@nestjs/bull';
+import { delay } from 'rxjs';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    BullModule.forRoot({
+      redis:process.env.REDIS_URL,
+      defaultJobOptions:{
+        removeOnComplete:100,
+        attempts:3,
+        removeOnFail:300,
+        backoff:{
+          type:"exponential",
+          delay:1000,
+        }
+      },
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [PrismaService,],
 })
 export class AppModule {}
