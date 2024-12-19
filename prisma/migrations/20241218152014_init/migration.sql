5f1257c7338eef8fb627b09054b6aaceb1ffa21d
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-19 17:13:34 -0300

    feat:Prisma Migration

---
+++ prisma/migrations/20241218152014_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateEnum
+CREATE TYPE "Tipo" AS ENUM ('Deposito', 'Saque', 'Transferencia');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transacoes" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "valor" DOUBLE PRECISION NOT NULL,
+    "createdById" INTEGER NOT NULL,
+    "tipo" "Tipo" NOT NULL,
+
+    CONSTRAINT "Transacoes_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Transacoes" ADD CONSTRAINT "Transacoes_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
