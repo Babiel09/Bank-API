334da4094178d000a6442e678a19614386ec6608
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-21 13:15:51 -0300

    feat: Post method

--- src/money/money.controller.ts
+++ src/money/money.controller.ts
@@ -1,6 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post, Res, Delete, Put, Body, Param, Query } from "@nestjs/common";
+import { MoneyService } from "./money.service";
+import { Response } from "express";
+import { CreateTransacao } from "./DTO/money.dto";
 
 @Controller("/task")
 export class MoneyController{
-    constructor(){};
+    constructor(private readonly moneyService:MoneyService){};
+
+    @Post("/v1")
+    public async postATransacao(@Res() res:Response, @Body()data:CreateTransacao):Promise<Response>{
+        try{
+            const criaNovaTransacao = await this.moneyService.Insert(data);
+            return res.status(201).send(criaNovaTransacao)
+        } catch(err){
+            console.error(err);
+            return res.status(500).json({server:err});
+        };
+    };
 };
