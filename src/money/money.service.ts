c5294b4c96529d987b7dc770da96156894fa1489
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-21 16:41:18 -0300

    feat: Added new heritage from the User Service class

--- src/money/money.service.ts
+++ src/money/money.service.ts
@@ -1,11 +1,12 @@
 import { InjectQueue } from "@nestjs/bull";
 import { Injectable, Logger } from "@nestjs/common";
 import { Prisma, Tipo, Transacoes } from "@prisma/client";
-import { DefaultArgs } from "@prisma/client/runtime/library";
+import { DefaultArgs } from "@Prisma/client/runtime/library";
 import { Queue } from "bull";
-import { PrismaService } from "prisma/prisma.service";
+import { PrismaService } from "Prisma/prisma.service";
 import { TRANSACOES_QUEUE } from "src/constants/constansts";
 import { CreateTransacao } from "./DTO/money.dto";
+import { UserService } from "src/user/user.service";
 
 
 
@@ -13,50 +14,51 @@ export interface MoneyThings{
     id:number;
     name:string;
     valor:number;
-    createdById:number;
+    forId:number;
     tipo: Tipo;
 };
 
 @Injectable()
-export class MoneyService{
-    private readonly prisma: Prisma.TransacoesDelegate<DefaultArgs>;
-    private readonly logger = new Logger(MoneyService.name);
-    constructor(private readonly pr:PrismaService, @InjectQueue(TRANSACOES_QUEUE) private readonly transacoes:Queue){
-        this.prisma = pr.transacoes;
+export class MoneyService extends UserService{
+    private readonly prisma2: Prisma.TransacoesDelegate<DefaultArgs>;
+    private readonly logger2 = new Logger(MoneyService.name);
+    constructor(private readonly pr2:PrismaService, @InjectQueue(TRANSACOES_QUEUE) private readonly transacoes:Queue){
+        super(pr2);
+        this.prisma2 = pr2.transacoes;
     };
 
-    public async Insert({name,valor,createdById,tipo}:CreateTransacao):Promise<Transacoes | null>{
+    public async Insert2({name,valor,forId,tipo}:CreateTransacao):Promise<Transacoes | null>{
         try{    
-            const tentaCriar = await this.prisma.create({
+            const tentaCriar = await this.prisma2.create({
                 data:{
                     name:name,
                     valor:valor,
-                    createdById:createdById,
+                    forId:forId,
                     tipo:tipo
                 },
             });
 
             if(!tentaCriar){
-                this.logger.error("We failed to create the transaction");
+                this.logger2.error("We failed to create the transaction");
                 return null;
             };
 
-            this.logger.log("Processando job");
+            this.logger2.log("Processando job");
 
            const job = await this.transacoes.add(TRANSACOES_QUEUE,{
                 transactionId:tentaCriar.id,
                 transactionName:tentaCriar.name,
                 transactionValue:tentaCriar.valor,
-                transactionCreatedBy:tentaCriar.createdById,
+                transactionFor:tentaCriar.forId,
                 transactionType:tentaCriar.tipo,
             });
 
-            this.logger.log(`\nJob foi carregado = \n${JSON.stringify(job)}\n`);
+            this.logger2.log(`Job foi carregado = \n${JSON.stringify(job)}\n`);
 
             return tentaCriar;
 
         } catch(err){
-            this.logger.error(err);
+            this.logger2.error(err);
             return null;
         };
     };
