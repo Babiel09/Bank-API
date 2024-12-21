277f3db46915207c80183b9b05983923376165d7
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-21 15:58:19 -0300

    fixed: Fixed the import of the money.processor

--- src/money/money.module.ts
+++ src/money/money.module.ts
@@ -2,7 +2,7 @@ import { Logger, Module } from "@nestjs/common";
 import { PrismaService } from "prisma/prisma.service";
 import { MoneyController } from "./money.controller";
 import { MoneyService } from "./money.service";
-import { MoneyProcessor } from "src/processor/money.processor";
+import { MoneyProcessor } from "src/money/processor/money.processor";
 import { BullModule } from "@nestjs/bull";
 import { TRANSACOES_QUEUE } from "src/constants/constansts";
 
