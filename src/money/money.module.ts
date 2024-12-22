25e6741a04657de8bb2e41847a1e30ce6044db16
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-22 16:07:26 -0300

    style: Making the user module in the ident

--- src/money/money.module.ts
+++ src/money/money.module.ts
@@ -11,7 +11,7 @@ import { PrismaModule } from "prisma/prisma.module";
 
 @Module({
     imports:[
-        UserModule,
+       UserModule,
        BullModule.registerQueue({
            name:TRANSACOES_QUEUE, 
        }), 
