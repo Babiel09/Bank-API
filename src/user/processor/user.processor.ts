d705efc9a0c7c6c5c4a5898af31e57ef105c0a31
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-25 18:37:53 -0300

    feat: Create a new Process to process the login

--- src/user/processor/user.processor.ts
+++ src/user/processor/user.processor.ts
@@ -1,8 +1,14 @@
-import { Processor } from "@nestjs/bull";
+import { Process, Processor } from "@nestjs/bull";
 import { USER_QUEUE } from "src/constants/constansts";
 import { UserService } from "../user.service";
+import { Job } from "bull";
 
 @Processor(USER_QUEUE)
 export class UserProcessor{
     constructor(private readonly userService:UserService){};
+
+    @Process("login")
+    private async makeTheJobLogin(job:Job){
+        await this.userService.Login(job.data);
+    };
 };
